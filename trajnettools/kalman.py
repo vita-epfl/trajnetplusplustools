b818e2d10dbbd45a25de3bdae22b470ef1135585
Sven Kreiss <me@svenkreiss.com>
2018-06-20 16:25:22 +0200

    kalman

--- trajnettools/kalman.py
+++ trajnettools/kalman.py
@@ -22,9 +22,14 @@ def predict(rows):
                                initial_state_mean=initial_state_mean)
     kf.em([(r.x, r.y) for r in rows[:9]], em_vars=['transition_matrices',
                                                    'observation_matrices'])
-    states, _ = kf.smooth([(r.x, r.y) for r in rows[:9]])
+    observed_states, _ = kf.smooth([(r.x, r.y) for r in rows[:9]])
+
+    # prepare predictions
     frame_diff = rows[1].frame - rows[0].frame
     first_frame = rows[-1].frame + frame_diff
     ped_id = rows[-1].pedestrian
+
+    # sample predictions (first sample corresponds to last state)
+    _, predictions = kf.sample(12 + 1, initial_state=observed_states[-1])
     return [Row(first_frame + i * frame_diff, ped_id, x, y)
-            for i, (x, y) in enumerate(kf.sample(12 + 1, initial_state=states[-1])[1][1:])]
+            for i, (x, y) in enumerate(predictions[1:])]
