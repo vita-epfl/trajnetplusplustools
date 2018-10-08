93628ad3a0bc2a90423ff0c021929671d3f4edf8
Rodolphe Farrando <rodolphe.farrando@epfl.ch>
2018-10-08 16:34:34 +0200

    add the collision metricand the fps

--- trajnettools/data.py
+++ trajnettools/data.py
@@ -2,4 +2,4 @@ from collections import namedtuple
 
 
 TrackRow = namedtuple('Row', ['frame', 'pedestrian', 'x', 'y'])
-SceneRow = namedtuple('Row', ['scene', 'pedestrian', 'start', 'end'])
+SceneRow = namedtuple('Row', ['scene', 'pedestrian', 'start', 'end', 'fps'])
