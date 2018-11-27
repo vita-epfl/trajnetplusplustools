9432486e5974856930f09d2cf7af62833cd2dbc0
Rodolphe Farrando <rodolphe.farrando@gmail.com>
2018-11-27 11:58:43 +0100

    update the name of the new feature

--- trajnettools/data.py
+++ trajnettools/data.py
@@ -1,5 +1,5 @@
 from collections import namedtuple
 
 
-TrackRow = namedtuple('Row', ['frame', 'pedestrian', 'x', 'y', 'pred_number'])
+TrackRow = namedtuple('Row', ['frame', 'pedestrian', 'x', 'y', 'prediction_number'])
 SceneRow = namedtuple('Row', ['scene', 'pedestrian', 'start', 'end', 'fps', 'tag'])
