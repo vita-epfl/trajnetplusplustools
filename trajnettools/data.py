8b254d740c6c7920a0b42828272b063c8ef76eee
Rodolphe Farrando <rodolphe.farrando@gmail.com>
2018-12-04 14:35:01 +0100

    Add default value to prediction number

--- trajnettools/data.py
+++ trajnettools/data.py
@@ -2,4 +2,5 @@ from collections import namedtuple
 
 
 TrackRow = namedtuple('Row', ['frame', 'pedestrian', 'x', 'y', 'prediction_number'])
+TrackRow.__new__.__defaults__ = (None, None, None, None, None)
 SceneRow = namedtuple('Row', ['scene', 'pedestrian', 'start', 'end', 'fps', 'tag'])
