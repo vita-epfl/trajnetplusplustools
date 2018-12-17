1f5c0ca6756a6067815f5d22cbe3e0d329c75923
Rodolphe Farrando <rodolphe.farrando@gmail.com>
2018-12-17 18:18:42 +0100

    clean collision function

--- trajnettools/metrics.py
+++ trajnettools/metrics.py
@@ -23,23 +23,22 @@ def collision(path1, path2, n_predictions=12, person_radius=0.1):
 
     assert len(path1) >= n_predictions
     path1 = path1[-n_predictions:]
-    common_frames1, common_frames2 = np.where(np.array(list(r1.frame == r2.frame for r1 in path1 for r2 in path2))
-                                              .reshape(len(path1), len(path2)) > 0)
+    frames1 = set([f1.frame for f1 in path1])
+    frames2 = set(f2.frame for f2 in path2)
+    common_frames = frames1.intersection(frames2)
 
     # If there is no interaction, there is no collision
-    if len(common_frames1) == 0:
+    if not common_frames:
         return False
 
-    path1 = [path1[i] for i in common_frames1]
-    path2 = [path2[i] for i in common_frames2]  # only potential collisions
-
+    path1 = [path1[i] for i in range(len(path1)) if path1[i].frame in common_frames]
+    path2 = [path2[i] for i in range(len(path2)) if path2[i].frame in common_frames]
 
     def getinsidepoints(p1, p2, parts=10):
         """return: equally distanced points between starting and ending "control" points"""
 
         return np.array((np.linspace(p1[0], p2[0], parts + 1),
                          np.linspace(p1[1], p2[1], parts + 1)))
-
 
     for i in range(len(path1) - 1):
         p1, p2 = [path1[i].x, path1[i].y], [path1[i + 1].x, path1[i + 1].y]
