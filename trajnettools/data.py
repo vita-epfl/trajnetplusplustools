1b9479a1f6ee54e62c9c848956f0f0142f748111
Parth Kothari <11539536+theDebugger811@users.noreply.github.com>
2019-08-28 10:24:39 +0200

    Added Scene_ID to track row

--- trajnettools/data.py
+++ trajnettools/data.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
 
-TrackRow = namedtuple('Row', ['frame', 'pedestrian', 'x', 'y', 'prediction_number'])
-TrackRow.__new__.__defaults__ = (None, None, None, None, None)
+TrackRow = namedtuple('Row', ['frame', 'pedestrian', 'x', 'y', 'prediction_number', 'scene_id'])
+TrackRow.__new__.__defaults__ = (None, None, None, None, None, None)
 SceneRow = namedtuple('Row', ['scene', 'pedestrian', 'start', 'end', 'fps', 'tag'])
