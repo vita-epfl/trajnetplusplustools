845e50e0e35e55ca03b35842990645e2cc8d2672
theDebugger811 <parthkothari811@gmail.com>
2019-09-25 12:54:14 +0200

    Pred Paths added

--- trajnettools/show.py
+++ trajnettools/show.py
@@ -86,3 +86,54 @@ def interaction_path(path, neigh, kf=None, output_file=None):
 
         # frame
         ax.legend()
+
+@contextmanager
+def predicted_paths(input_paths, pred_paths, output_file=None):
+    """Context to plot paths."""
+    with canvas(output_file, figsize=(8, 8)) as ax:
+        ax.grid(linestyle='dotted')
+        ax.set_aspect(1.0, 'datalim')
+        ax.set_xlabel('x [m]')
+        ax.set_ylabel('y [m]')
+
+        yield ax
+
+        # primary
+        xs = [r.x for r in input_paths[0]]
+        ys = [r.y for r in input_paths[0]]
+        # track
+        ax.plot(xs, ys, color='black', linestyle='solid', label='primary',
+                marker='o', markersize=2.5, zorder=1.9)
+        # markers
+        ax.plot(xs[0:1], ys[0:1], color='black', marker='x', label='start',
+                linestyle='None', zorder=0.9)
+        ax.plot(xs[-1:], ys[-1:], color='black', marker='o', label='end',
+                linestyle='None', zorder=0.9)
+        
+
+        # neigh tracks
+        for ped_rows in input_paths[1:]:
+            xs = [r.x for r in ped_rows]
+            ys = [r.y for r in ped_rows]
+            # markers
+            ax.plot(xs[0:1], ys[0:1], color='black', marker='x', linestyle='None')
+            ax.plot(xs[-1:], ys[-1:], color='black', marker='o', linestyle='None')
+            # track
+            ax.plot(xs, ys, color='black', linestyle='dotted')
+
+        # primary
+        for name, primary in pred_paths.items():
+            xs = [r.x for r in primary]
+            ys = [r.y for r in primary]
+            # track
+            ax.plot(xs, ys, linestyle='solid', label=name,
+                    marker='o', markersize=2.5, zorder=1.9)
+            # markers
+            ax.plot(xs[0:1], ys[0:1], color='black', marker='x', label='start',
+                    linestyle='None', zorder=0.9)
+            ax.plot(xs[-1:], ys[-1:], color='black', marker='o', label='end',
+                    linestyle='None', zorder=0.9)
+
+
+        # frame
+        ax.legend()
